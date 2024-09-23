798032c034cd3d258ccaf7aa63887d2135b49eb6
Jazab Ahmad <jazabshoukat72@gmail.com>
2024-09-23 20:30:18 +0500

    improve layout

--- src/components/MainCard.jsx
+++ src/components/MainCard.jsx
@@ -4,6 +4,13 @@ import mainImg from '../assets/mainCard.jpg'
 import { Link } from 'react-router-dom'
 
 export default function MainCard({ data }) {
+    const shortenText = (htmlContent, limit) => {
+        const tempElement = document.createElement("div");
+        tempElement.innerHTML = htmlContent;
+        const textContent = tempElement.textContent || tempElement.innerText || "";
+        const shortened = textContent.split(" ").slice(0, limit).join(" ");
+        return shortened + "...";
+    };
     return (
         <Link to={`/detail?id=${data.id}`} className='mainCard'>
             <div className="mainCardImg">
@@ -21,8 +28,7 @@ export default function MainCard({ data }) {
                     <p><i className="fa-regular fa-calendar-days"></i>17 July</p>
                 </div>
                 <div className="mainCardText">
-                    <p>Cursus iaculis etiam in In nullam donec sem sed consequat scelerisque
-                        nibh amet, massa egestas risus, gravida vel amet, imperdiet ...</p>
+                    <p>{shortenText(data.content, 20)}...</p>
                 </div>
             </div>
         </Link>
